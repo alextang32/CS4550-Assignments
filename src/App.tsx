55d2bec806dc740c7a8d5996447577b3f0d48f10
Alex Tang <alxhtang@gmail.com>
2024-09-23 20:13:18 -0400

    finished dashboard

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Labs from "./Labs";
 import { HashRouter, Route, Routes, Navigate } from "react-router-dom";
 import logo from './logo.svg';
+import Kanbas from "./Kanbas";
 import './App.css';
 
 function App() {
@@ -9,8 +10,9 @@ function App() {
     <HashRouter>
     <div>
     <Routes>
-    <Route path="/" element={<Navigate to="Labs" />} />
+    <Route path="/" element={<Navigate to="Kanbas" />} />
           <Route path="/Labs/*" element={<Labs />} />
+          <Route path="/Kanbas/*" element={<Kanbas />} />
         </Routes>
     </div>
     </HashRouter>
