8340ce02e011d961a7ee5d2d7890253f0b5349cd
Alex Tang <alxhtang@gmail.com>
2024-10-22 18:51:48 -0400

    3.1

--- src/Kanbas/Navigation.tsx
+++ src/Kanbas/Navigation.tsx
@@ -1,76 +1,36 @@
-import React, { useState } from 'react';
 import { AiOutlineDashboard } from "react-icons/ai";
 import { IoCalendarOutline } from "react-icons/io5";
 import { LiaBookSolid, LiaCogSolid } from "react-icons/lia";
 import { FaInbox, FaRegCircleUser } from "react-icons/fa6";
-import { Link } from "react-router-dom";
-import "./styles.css";
+import { Link, useLocation } from "react-router-dom";
 export default function KanbasNavigation() {
-  const [selectedLink, setSelectedLink] = useState<string>("/Kanbas/Dashboard");
-
-  const getLinkClass = (link: string): string => (
-    link === selectedLink 
-      ? "list-group-item text-center border-0 bg-white text-danger" 
-      : "list-group-item text-white bg-black text-center border-0" 
-  );
-
-
-  const getIconClass = (link: string): string => {
-    if (link === "/Kanbas/Account") {
-      return link === selectedLink ? "fs-1 text-danger" : "fs-1 text-white"; 
-    }
-    return "fs-1 text-danger"; 
-  };
-
+  const { pathname } = useLocation();
+  const links = [
+    { label: "Dashboard", path: "/Kanbas/Dashboard", icon: AiOutlineDashboard },
+    { label: "Courses",   path: "/Kanbas/Dashboard", icon: LiaBookSolid },
+    { label: "Calendar",  path: "/Kanbas/Calendar",  icon: IoCalendarOutline },
+    { label: "Inbox",     path: "/Kanbas/Inbox",     icon: FaInbox },
+    { label: "Labs",      path: "/Labs",             icon: LiaCogSolid },
+  ];
   return (
-    <div id="wd-kanbas-navigation" style={{ width: 120 }} 
+    <div id="wd-kanbas-navigation" style={{width: 120}}
          className="list-group rounded-0 position-fixed bottom-0 top-0 d-none d-md-block bg-black z-2">
-      <a href="https://www.northeastern.edu/" className="list-group-item bg-black border-0 text-center"
-         id="wd-neu-link" target="_blank" rel="noopener noreferrer">
-        <img src="/images/NEU.png" width="75px" alt="NEU Logo" />
-      </a>
-      
-      <Link to="/Kanbas/Account" 
-            id="wd-account-link" 
-            className={getLinkClass("/Kanbas/Account")}
-            onClick={() => setSelectedLink("/Kanbas/Account")}>
-        <FaRegCircleUser className={getIconClass("/Kanbas/Account")} /><br />Account
+      <a id="wd-neu-link" target="_blank" href="https://www.northeastern.edu/"
+        className="list-group-item bg-black border-0 text-center">
+        <img src="/images/NEU.png" width="75px" /></a>
+      <Link to="/Kanbas/Account" className={`list-group-item text-center border-0 bg-black
+            ${pathname.includes("Account") ? "bg-white text-danger" : "bg-black text-white"}`}>
+        <FaRegCircleUser className={`fs-1 ${pathname.includes("Account") ? "text-danger" : "text-white"}`} />
+        <br />
+        Account
       </Link>
-
-      <Link to="/Kanbas/Dashboard" 
-            id="wd-dashboard-link" 
-            className={getLinkClass("/Kanbas/Dashboard")}
-            onClick={() => setSelectedLink("/Kanbas/Dashboard")}>
-        <AiOutlineDashboard className={getIconClass("/Kanbas/Dashboard")} /><br />Dashboard
-      </Link>
-
-      <Link to="/Kanbas/Courses" 
-            id="wd-course-link" 
-            className={getLinkClass("/Kanbas/Courses")}
-            onClick={() => setSelectedLink("/Kanbas/Courses")}>
-        <LiaBookSolid className={getIconClass("/Kanbas/Courses")} /><br />Courses
-      </Link>
-
-      <Link to="/Kanbas/Calendar" 
-            id="wd-calendar-link" 
-            className={getLinkClass("/Kanbas/Calendar")}
-            onClick={() => setSelectedLink("/Kanbas/Calendar")}>
-        <IoCalendarOutline className={getIconClass("/Kanbas/Calendar")} /><br />Calendar
-      </Link>
-
-      <Link to="/Kanbas/Inbox" 
-            id="wd-inbox-link" 
-            className={getLinkClass("/Kanbas/Inbox")}
-            onClick={() => setSelectedLink("/Kanbas/Inbox")}>
-        <FaInbox className={getIconClass("/Kanbas/Inbox")} /><br />Inbox
-      </Link><br/>
-
-      <Link to="/Labs" 
-            id="wd-labs-link"
-            className={getLinkClass("/Labs")}
-            onClick={() => setSelectedLink("/Labs")}>
-        <LiaCogSolid className={getIconClass("/Labs")} /><br />Labs
-      </Link><br/>
+      {links.map((link) => (
+        <Link key={link.path} to={link.path} className={`list-group-item bg-black text-center border-0
+              ${pathname.includes(link.label) ? "text-danger bg-white" : "text-white bg-black"}`}>
+          {link.icon({ className: "fs-1 text-danger"})}
+          <br />
+          {link.label}
+        </Link>
+      ))}
     </div>
-  );
-}
+);}
