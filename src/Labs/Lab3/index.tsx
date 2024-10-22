95b4ffaad9b9ab634dde57bfa6ad63aab5e631b9
Alex Tang <alxhtang@gmail.com>
2024-10-22 14:52:18 -0400

    2.4

--- src/Labs/Lab3/index.tsx
+++ src/Labs/Lab3/index.tsx
@@ -9,6 +9,22 @@ import LegacyFunctions from "./LegacyFunctions";
 import ArrowFunctions from "./ArrowFunctions";
 import ImpliedReturn from "./ImpliedReturn";
 import TemplateLiterals from "./TemplateLiterals";
+import SimpleArrays from "./SimpleArrays";
+import ArrayIndexAndLength from "./ArrayIndexAndLength";
+import AddingAndRemovingToFromArrays from "./AddingAndRemovingToFromArrays";
+import ForLoops from "./ForLoops";
+import MapFunction from "./MapFunction";
+import FindFunction from "./FindFunction";
+import FindIndex from "./FindIndex";
+import FilterFunction from "./FilterFunction";
+import JsonStringify from "./JsonStringify";
+import House from "./House";
+import TodoItem from "./todos/TodoItem";
+import TodoList from "./todos/TodoList";
+import Spreading from "./Spreading";
+import Destructing from "./Destructing";
+import FunctionDestructing from "./FunctionDestructing";
+import DestructingImports from "./DestructingImports";
 export default function Lab3() {
   return(
     <div id="wd-lab3">
@@ -24,6 +40,22 @@ export default function Lab3() {
       <ArrowFunctions/>
       <ImpliedReturn/>
       <TemplateLiterals/>
+      <SimpleArrays/>
+      <ArrayIndexAndLength/>
+      <AddingAndRemovingToFromArrays/>
+      <ForLoops/>
+      <MapFunction/>
+      <FindFunction/>
+      <FindIndex/>
+      <FilterFunction/>
+      <JsonStringify/>
+      <House/>
+      <TodoItem/>
+      <TodoList/>
+      <Spreading/>
+      <Destructing/>
+      <FunctionDestructing/>
+      <DestructingImports/>
     </div>
   );
 }
