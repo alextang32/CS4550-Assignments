9398c75ba0d315bbac33e20064d0c729fd1535c6
Alex Tang <alxhtang@gmail.com>
2024-12-08 10:51:29 -0500

    a6

--- src/Kanbas/Account/Navigation.tsx
+++ src/Kanbas/Account/Navigation.tsx
@@ -1,17 +1,17 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import { useSelector } from "react-redux";
+
 export default function AccountNavigation() {
   const { currentUser } = useSelector((state: any) => state.accountReducer);
+  const { pathname } = useLocation();
   const links = currentUser ? ["Profile"] : ["Signin", "Signup"];
+  const active = (path: string) => (pathname.includes(path) ? "active" : "");
   return (
-      <div id="wd-courses-navigation" className="wd list-group fs-5 rounded-0">
-      <Link to="/Kanbas/Account/Signin" id="wd-signin-link"
-        className="list-group-item active border border-0"> Signin </Link><br />
-      <Link to="/Kanbas/Account/Signup" id="wd-signup-link"
-        className="list-group-item text-danger border border-0"> Signup </Link><br />
-      <Link to="/Kanbas/Account/Profile" id="wd-profile-link"
-        className="list-group-item text-danger border border-0"> Profile </Link><br />
+    <div id="wd-account-navigation">
+      {links
+        .flatMap(link =>
+          [<Link to={`/Kanbas/Account/${link}`}>{link}</Link>, <br/>])}
+      {currentUser && currentUser.role === "ADMIN" && (
+        <Link to={`/Kanbas/Account/Users`} className={`list-group-item ${active("Users")}`}> Users </Link> )}
     </div>
-
-    
 );}
--- src/Kanbas/index.tsx
+++ src/Kanbas/index.tsx
@@ -10,7 +10,52 @@ import ProtectedRoute from "./Account/ProtectedRoute";
 import Session from "./Account/session";
 import { useSelector } from "react-redux";
 import * as courseClient from "./Courses/client";
+
 export default function Kanbas() {
+  const [enrolling, setEnrolling] = useState<boolean>(false);
+  const findCoursesForUser = async () => {
+    try {
+      const courses = await userClient.findCoursesForUser(currentUser._id);
+      setCourses(courses);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  const fetchCourses = async () => {
+    try {
+      const allCourses = await courseClient.fetchAllCourses();
+      const enrolledCourses = await userClient.findCoursesForUser(
+        currentUser._id
+      );
+      const courses = allCourses.map((course: any) => {
+        if (enrolledCourses.find((c: any) => c._id === course._id)) {
+          return { ...course, enrolled: true };
+        } else {
+          return course;
+        }
+      });
+      setCourses(courses);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  const updateEnrollment = async (courseId: string, enrolled: boolean) => {
+    if (enrolled) {
+      await userClient.enrollIntoCourse(currentUser._id, courseId);
+    } else {
+      await userClient.unenrollFromCourse(currentUser._id, courseId);
+    }
+    setCourses(
+      courses.map((course) => {
+        if (course._id === courseId) {
+          return { ...course, enrolled: enrolled };
+        } else {
+          return course;
+        }
+      })
+    );
+  };
+ 
   const updateCourse = async () => {
     await courseClient.updateCourse(course);
     setCourses(courses.map((c) => {
@@ -24,27 +69,24 @@ export default function Kanbas() {
     _id: "1234", name: "New Course", number: "New Number",
     startDate: "2023-09-10", endDate: "2023-12-15", description: "New Description",
   });
-  const addNewCourse = () => {
+  const addNewCourse = async () => {
     setCourses([...courses, { ...course, _id: new Date().getTime().toString() }]);
   };
-  const deleteCourse = async (courseId: any) => {
-    const status = await courseClient.deleteCourse(courseId);
+  const deleteCourse = async (courseId: string) => {
+    const status = await courseClient.deleteCourse(courseId); 
     setCourses(courses.filter((course) => course._id !== courseId));
   };
  
 
   const { currentUser } = useSelector((state: any) => state.accountReducer);
-  const fetchCourses = async () => {
-    try {
-      const courses = await userClient.findMyCourses();
-      setCourses(courses);
-    } catch (error) {
-      console.error(error);
-    }
-  };
+
   useEffect(() => {
-    fetchCourses();
-  }, [currentUser]);
+    if (enrolling) {
+      fetchCourses();
+    } else {
+      findCoursesForUser();
+    }
+  }, [currentUser, enrolling]);
 
   return (
     <Session>
@@ -60,7 +102,10 @@ export default function Kanbas() {
               setCourse={setCourse}
               addNewCourse={addNewCourse}
               deleteCourse={deleteCourse}
-              updateCourse={updateCourse}/></ProtectedRoute>} />
+              updateCourse={updateCourse}
+              enrolling={enrolling} setEnrolling={setEnrolling}
+              updateEnrollment={updateEnrollment}
+              /></ProtectedRoute>} />
         <Route path="/Courses/:cid/*" element={<ProtectedRoute><Courses 
          courses={courses}/></ProtectedRoute>} />
               <Route path="/Calendar" element={<h1>Calendar</h1>} />
